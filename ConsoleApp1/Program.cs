7a514308e1b2dfd5041c9ee566b20dcfc0676ae1
Simeon1902 <150514682+Simeon1902@users.noreply.github.com>
2023-11-11 11:35:40 +0000

    First code exercises

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿int a = 9;
+int b = 12;
+int licep = a*b;
+int obikp = 2*(a+b);
+Console.WriteLine($"liceto na pravoukulnika e {licep}");
+Console.WriteLine($"Obikolkata na pravoukulnika e {obikp}");
+// 2. trigulunik
+double s1 = 3.5d;
+double v = 4.5d;
+double licet = (s1*v)/2;
+double obikt = 3*v;
+Console.WriteLine($"liceto na triugulnika e {licet}");
+Console.WriteLine($"Obikolkata na triugulnika e {obikt}");
+// 3. sravnenie lice
+if (licep>licet)
+{
+    Console.WriteLine("Liceto na pravougulnika e po-golqmo ot liceto na triugulnika");
+}
+else 
+{
+    Console.WriteLine("Liceto na triugulnika e po-golqmo ot liceto na pravougulnika" );
+}
+// 4. sravnenie obikolka
+if (obikp>obikt)
+{
+    Console.WriteLine("Obikolkata na pravougulnika e po-golqma ot obikolkata na triugulnika");
+}
+else 
+{
+    Console.WriteLine("Obikolkata na triugulnika e po-golqma ot obikolkata na pravougulnika" );
+}
+// 5. sravnenie 
+if (licep>licet && obikp>obikt)
+{
+    Console.WriteLine("Liceto i obikolkata na pravougulnika sa po-golemi");
+}
+else
+{
+    Console.WriteLine("Liceto i obikolkata na triugulnika sa po-golemi");
+}
+// 6. godini
+Console.WriteLine("Vuvedi godini");
+int age;
+string ab = Console.ReadLine();
+age = int.Parse(ab);
+if (age<14)
+{
+    Console.WriteLine("Ti si maloleten");
+}
+else if(age<18)
+
+    Console.WriteLine("Ti si nepulnoleten");
+}
+else
+{    
+    Console.WriteLine("Ti si pulnoleten");
+}
+
+
